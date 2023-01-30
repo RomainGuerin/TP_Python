973755e1791bc99b449cf798ca202a9b86c31587
Romain Guerin <romainguerin911@gmail.com>
2023-01-30 16:42:48 +0100

    Update Robot

--- Robot.py
+++ Robot.py
@@ -2,7 +2,8 @@ from Bipede import Bipede
 
 class Robot(Bipede):
 
-    def __init__(self, id, marque):
+    def __init__(self, nom, sexe, id, marque):
+        super().__init__(nom, sexe)
         self.id = id
         self.marque = marque
 
@@ -13,10 +14,10 @@ class Robot(Bipede):
         pass
 
     def __str__(self):
-        return (f"{self.id}, {self.marque}")
+        return (f"Robot n°{self.id} ({self.sexe})\nNom d'usage : {self.nom}\nFabriqué par : {self.marque}\n")
 
 if __name__ == "__main__":
-    robot_1 = Robot(id=1, marque="Tesla")
-    robot_2 = Robot(id=2, marque="Boston Dynamics")
+    robot_1 = Robot(nom="IA", sexe="Binaire", id=1, marque="Tesla")
+    robot_2 = Robot(nom="VITE", sexe="Quantique", id=2, marque="Boston Dynamics")
     print(robot_1)
     print(robot_2)
